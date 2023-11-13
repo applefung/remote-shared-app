dd0713d17f65d88ac13e7a4769d3ff06dea72ba1
amber.fung <amber.fung@lalamove.com>
2023-11-13 14:34:59 +0800

    feat: remove unused bug

--- src/stores/global.ts
+++ src/stores/global.ts
@@ -14,10 +14,3 @@ export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
 
-// expose the store
-declare global {
-  interface Window {
-    store: typeof store;
-  }
-}
-window.store = store;
